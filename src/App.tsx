517b0b2f38e59380c8e0b497326c0128b4150200
angelalves <angeltrashangel@gmail.com>
2024-10-08 13:48:14 -0300

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HookFormZod from "./components/HookFormZod";
+
+function App() {
+  return (
+    <div className="flex w-screen h-screen justify-center items-center bg-violet-200">
+      <HookFormZod />
+    </div>
+  );
+}
+
+export default App;
